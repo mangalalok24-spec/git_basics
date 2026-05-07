efda9fb027205b688e12c9a57dcf14611d521e68
Alok Mangal <mangalalok24@gmail.com>
2026-05-07 16:34:45 +0530

    add method added

---
+++ src/com/git/Main.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+
+}
